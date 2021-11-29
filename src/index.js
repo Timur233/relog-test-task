abbd500f47d635795a68100a7c780db71d03e9c3
Iskandarov Timur <timur.iskandarov96@gmail.com>
2021-11-29 18:18:45 +0600

    Верстка

--- src/index.js
+++ src/index.js
@@ -9,11 +9,27 @@ import HelloWorld from './components/HelloWorld';
 render(
     <div className="container">
         <div className="grid">
-            <div className="grid__col grid__col--3">
-                <h3>SideBar Saebal</h3>
+            <div className="grid__col grid__col--3 sidebar">
+                <h3 className="order-title">Заказы:</h3>
+                <div className="order-list">
+                    <div className="order-list__item order">
+                        <h4 className="order__client-title">Research Direct Consulting</h4>
+                        <span className="order__client-phone">(828)463-9921</span>
+                        <span className="order__order-type">Доставка</span>
+                        <span className="order__price">Цена: <span className="order__price-span">7 579 тг.</span></span>
+                    </div>
+                    <div className="order-list__item order">
+                        <h4 className="order__client-title">Signal Systems Virtual</h4>
+                        <span className="order__client-phone">(570)211-4294</span>
+                        <span className="order__order-type order__order-type--green">Забор</span>
+                        <span className="order__price">Цена: <span className="order__price-span">8 449 тг.</span></span>
+                    </div>
+                </div>
             </div>
             <div className="grid__col grid__col--9">
-                <h3>Main Contents SKA</h3>
+                <div className="map-container">
+                    123
+                </div>
                 <HelloWorld />
             </div>
         </div>
